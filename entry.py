bd4b3761aa42798eb796bda5c16a13ede3f7e1b1
Eilidh Southren <eilidhsouthren@gmail.com>
2024-08-18 22:06:48 +0100

    Update entry.py

--- entry.py
+++ entry.py
@@ -15,13 +15,13 @@ dfs = []
 for year_window in [1,2,3]:
     df = prepare_model_data(year_window)
     dfs.append(df)
-    # print(f"Number of rows in df for year {year_window}: {len(df)}")
-    # significance_testing(df)
+    print(f"Number of rows in df for year {year_window}: {len(df)}")
+    significance_testing(df)
 
-    # run_logistic_regression_model(df, year_window, logistic_regression_metrics)
-    # run_random_forest_model(df, year_window, random_forest_metrics)
+    run_logistic_regression_model(df, year_window, logistic_regression_metrics)
+    run_random_forest_model(df, year_window, random_forest_metrics)
     run_support_vector_machine_model(df, year_window, svm_metrics)
 
-# plot_all_model_metrics(logistic_regression_metrics)
-# plot_all_model_metrics(random_forest_metrics)
-# plot_all_model_metrics(svm_metrics)
+plot_all_model_metrics(logistic_regression_metrics)
+plot_all_model_metrics(random_forest_metrics)
+plot_all_model_metrics(svm_metrics)
